ab2de864f050b03ead3da6eece1ed22cf6fb3b48
Tomasz Olejniczak <t-olejniczak@wp.pl>
2018-12-03 02:02:28 +0100

    Addition course of the game to modal

--- js/index.js
+++ js/index.js
@@ -19,7 +19,6 @@ var params = {
 	progress: []
 }
 
-
 function main(){
   moveButtons.forEach(function(button) {
     var userChoice = button.dataset.move;
@@ -31,9 +30,8 @@ function main(){
 }
 
 function game(userChoice) {
-
-	var computerChoice = getComputerChoice();
-
+	params.numberOfRounds++;
+	var computerChoice = getComputerChoice()
 	switch(userChoice + computerChoice) {
 		case "RockRock":
 		case "PaperPaper":
@@ -60,47 +58,72 @@ function getComputerChoice() {
 }
 
 function draw(userChoice, computerChoice) {
- 	params.numberOfRounds++;
   	roundsCounter_p.innerHTML = "Number of rounds: " + params.numberOfRounds;
 	computerScore_span.innerHTML = params.computerScore;
 	userScore_span.innerHTML = params.userScore;
-	var smallerUserWord = "user ".fontsize(4);
+	var smallerUserWord = (userName + " ").fontsize(4);
 	var smallerCompWord = " comp".fontsize(4);
 	result_p.innerHTML = smallerUserWord + userChoice + " vs " + computerChoice + smallerCompWord + "<br>It is a draw!";
+	var result = "draw";
 	document.getElementById(userChoice).classList.add('gray-glow');
 	setTimeout(function() {document.getElementById(userChoice).classList.remove('gray-glow')}, 400);
+	var objectResults = {
+		number: params.numberOfRounds,
+		playerMove: userChoice,
+		computerMove: computerChoice,
+		result: result,
+		gameResult: params.userScore+":"+params.computerScore,
+	}
+	params.progress[params.numberOfRounds] = objectResults;
 }
 
 function win(userChoice, computerChoice) {
-  	params.numberOfRounds++;
+	params.userScore++;
   	roundsCounter_p.innerHTML = "Number of rounds: " + params.numberOfRounds;
-  	params.userScore++;
 	userScore_span.innerHTML = params.userScore;
 	computerScore_span.innerHTML = params.computerScore;
-	var smallerUserWord = "user ".fontsize(4);
+	var smallerUserWord = (userName + " ").fontsize(4);
 	var smallerCompWord = " comp".fontsize(4);
 	result_p.innerHTML = smallerUserWord + userChoice + " vs " + computerChoice + smallerCompWord + "<br>You won!";
+	var result = "win";
 	document.getElementById(userChoice).classList.add('green-glow');
 	setTimeout(function() {document.getElementById(userChoice).classList.remove('green-glow')}, 400);
+	var objectResults = {
+		number: params.numberOfRounds,
+		playerMove: userChoice,
+		computerMove: computerChoice,
+		result: result,
+		gameResult: params.userScore+":"+params.computerScore,
+	}
+	params.progress[params.numberOfRounds] = objectResults;
   endGame();
 }
 
 function lose(userChoice, computerChoice) {
-  	params.numberOfRounds++;
+	params.computerScore++;
   	roundsCounter_p.innerHTML = "Number of rounds: " + params.numberOfRounds;
- 	params.computerScore++;
 	computerScore_span.innerHTML = params.computerScore;
 	userScore_span.innerHTML = params.userScore;
-	var smallerUserWord = "user ".fontsize(4);
+	var smallerUserWord = (userName + " ").fontsize(4);
 	var smallerCompWord = " comp".fontsize(4);
 	result_p.innerHTML = smallerUserWord + userChoice + " vs " + computerChoice + smallerCompWord + "<br>You lost!";
+	var result = "lost";
 	document.getElementById(userChoice).classList.add('red-glow');
 	setTimeout(function() {document.getElementById(userChoice).classList.remove('red-glow')}, 400);
+	var objectResults = {
+		number: params.numberOfRounds,
+		playerMove: userChoice,
+		computerMove: computerChoice,
+		result: result,
+		gameResult: params.userScore+":"+params.computerScore,
+	}
+	params.progress[params.numberOfRounds] = objectResults;
   endGame();
 }
 
 function newGame(){
 	newGame_div.addEventListener('click', function(){
+		cleanTable();
 		howManyWins();
 	})
   	main();
@@ -164,7 +187,9 @@ function endGame(){
     result_p.innerHTML = "";
     userName_h = userName;
     modal_h.innerHTML = userName + "<br>You won the ENTIRE game!";
+    makeTableFromProgress();
     showModal();
+    params.progress = [];
   }
   else if(numberOfGames == params.computerScore){
     choices_div.classList.add('choices-disabled');
@@ -173,7 +198,9 @@ function endGame(){
     result_p.innerHTML = "";
     userName_h = userName;
 	modal_h.innerHTML = userName + "<br>You lose the ENTIRE game!";
+	makeTableFromProgress();
     showModal();
+    params.progress = [];
   }
 }
 
@@ -189,13 +216,13 @@ choices_div.classList.add('choices-disabled');
 	var showModal = function(){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.add('show');
-		modal.classList.add('show'); //2
+		modal.classList.add('show');
 	};
 
 	var hideModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.remove('show');
-		this.parentElement.classList.remove('show'); //3
+		this.parentElement.classList.remove('show');
 	};
 	
 	var closeButtons = document.querySelectorAll('.close');
@@ -213,3 +240,43 @@ choices_div.classList.add('choices-disabled');
 			event.stopPropagation();
 		});
 	}
+
+function makeTableFromProgress() {
+	var resultsTable = document.querySelector('.resultsTable');
+    resultsTable.innerHTML = `
+    	<div class="resultsTable">
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Player move</th>
+                        <th>Computer move</th>
+                        <th>Result</th>
+                        <th>Game score</th>
+                    </tr>
+                </thead>
+                <tbody id="resultsTableBody"></tbody>
+            </table>
+        </div>`
+
+    var resultsTableBody = document.querySelector('#resultsTableBody');
+    var rows = params.progress.length;
+    for (i = 1; i<rows; i++) {
+      var row = resultsTableBody.insertRow(i-1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+    cell1.innerHTML = params.progress[i]['number'];
+    cell2.innerHTML = params.progress[i]['playerMove'];
+    cell3.innerHTML = params.progress[i]['computerMove'];
+    cell4.innerHTML = params.progress[i]['result'];
+    cell5.innerHTML = params.progress[i]['gameResult'];
+  }
+};
+
+function cleanTable() {
+    var resultsTable = document.querySelector('.resultsTable');
+    resultsTable.innerHTML = '';
+};
